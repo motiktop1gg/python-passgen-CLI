54d941d3f0cb0e8beb3bf66a0493a336419de82e
motiktop1gg <127473681+motiktop1gg@users.noreply.github.com>
2026-03-14 14:51:27 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,4 +1,4 @@
-import random, tkinter as tk
+import random
 import datetime as dt
 
 all_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "@", "!", "$", "%", "&", "^", "*", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
